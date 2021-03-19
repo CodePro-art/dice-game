850dacd908fe438baa9287f38e4c44567a640951
Netanel Mazuz <netazuz@gmail.com>
2021-03-19 16:07:26 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+// import ReactDice from 'react-dice-complete';
+// import 'react-dice-complete/dist/react-dice-complete.css';
+import DiceGame from './DiceGame';
+import * as THREE from "three";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiceGame/>
     </div>
   );
 }
---
+++ src/DiceGame.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class DiceGame extends Component {
+  render() {
+    return (
+      <div className="dice-game">
+        
+      </div>
+    )
+  }
+}
